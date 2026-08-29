60e4c6d5ee45324dd2335b9c2ef530b95b0acfbc
vivekpinto <vivekpinto5@gmail.com>
2026-08-29 15:54:18 +0530

    Removed the config file

--- frontend/src/pages/SectionPage.jsx
+++ frontend/src/pages/SectionPage.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { API_BASE_URL } from '../config';
 import ColorContrastChecker from '../components/ColorContrastChecker';
 
 // Temporary mapping for section info
